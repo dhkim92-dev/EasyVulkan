294805cc9dea4c57fc6a928941ee4d40bbe3edd7
dhkim92-dev <dhkim92.dev@gmail.com>
2024-01-02 17:27:06 +0900

    build directory removed

--- build/easy_vulkan/include/builder/ev_builder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EV_BUILDER_H__
-#define __EV_BUILDER_H__
-
-#include "ev_device_ci.h"
-#include "ev_instance_ci.h"
-#include "ev_queue_ci.h"
-
-#endif
--- build/easy_vulkan/include/easy_vulkan.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EASY_VULKAN_H__
-#define __EASY_VULKAN_H__
-
-#include "ev_instance.h"
-#include "ev_device.h"
-#include "ev_utility.h"
-#include "builder/ev_builder.h"
-
-#endif
